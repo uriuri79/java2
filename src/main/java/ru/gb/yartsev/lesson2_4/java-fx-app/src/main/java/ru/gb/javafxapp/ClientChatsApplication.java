373ca536e2845eed68f2c4a5a1b4efe562d4010b
Yartsev Yuri <yartsev_yu@mail.ru>
2022-06-13 00:34:43 +0300

    complete task

--- src/main/java/ru/gb/yartsev/lesson2_4/java-fx-app/src/main/java/ru/gb/javafxapp/ClientChatsApplication.java
+++ src/main/java/ru/gb/yartsev/lesson2_4/java-fx-app/src/main/java/ru/gb/javafxapp/ClientChatsApplication.java
@@ -7,15 +7,16 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ClientChatsApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(ClientChatsApplication.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 480, 320);
+        stage.setTitle("Clients Chat Window");
         stage.setScene(scene);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
